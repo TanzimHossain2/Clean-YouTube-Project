414b2760752f086e399888128713bccb5ba4f093
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-11-09 19:56:00 +0600

    Document code in usePlaylist.jsx and index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,26 @@
 import axios from "axios";
 
-const KEY = import.meta.env.VITE_API_KEY;
+/**
+ * @description Fetches playlist items from YouTube API for a given playlist ID.
+ * @param {string} playlistId - ID of the YouTube playlist.
+ * @param {string} pageToken - Token for paginating through results.
+ * @param {Array} result - Array to accumulate playlist items.
+ * @returns {Array} - Array of playlist items with content details and snippet information.
+ */
+const getPlaylist = async (playlistId, pageToken = '', result = []) => {
 
+    const KEY = import.meta.env.VITE_API_KEY; // API key stored in .env file
+  const URL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${KEY}&part=contentDetails,snippet,id&maxResults=50&playlistId=${playlistId}&pageToken=${pageToken}`;
 
-const getPlaylist = async (playlistId, pageToken='', result = []) => {
-    const URL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${KEY}&part=contentDetails,snippet,id&maxResults=50&playlistId=${playlistId}&pageToken=${pageToken}`;
+  const { data } = await axios.get(URL);
+  result = [...result, ...data.items];
 
-    const { data } = await axios.get(URL);
-    result = [...result, ...data.items];
-    if(data.nextPageToken) {
-        result = getPlaylist(playlistId, data.nextPageToken, result );
-    }
+  if (data.nextPageToken) {
+    // If there are more pages, recursively fetch them
+    result = await getPlaylist(playlistId, data.nextPageToken, result);
+  }
 
-    return result;
-
-}
+  return result;
+};
 
 export default getPlaylist;
-
--- src/hooks/usePlaylist.jsx
+++ src/hooks/usePlaylist.jsx
@@ -1,21 +1,55 @@
 import { useState } from "react";
 import getPlaylist from "../api";
 
+/**
+ * @description Custom React Hook for managing playlists and fetching data from an API.
+ * @returns {{
+ *   playlists: Object,
+ *   favourites: Array,
+ *   recentPlaylists: Array,
+ *   getPlaylistById: Function,
+ *   addToFavorites: Function,
+ *   addToRecent: Function
+ * }}
+ * @example
+ * const { getPlaylistById, playlists, addToFavorites, addToRecent } = usePlaylist();
+ *
+ * // Fetch and log a playlist by ID
+ * getPlaylistById("PL_XxuZqN0xVD0op-QDEgyXFA4fRPChvkl");
+ * console.log(playlists);
+ *
+ * // Add a playlist to favorites and recent playlists
+ * addToFavorites("PL_XxuZqN0xVD0op-QDEgyXFA4fRPChvkl");
+ * addToRecent("PL_XxuZqN0xVD0op-QDEgyXFA4fRPChvkl");
+ */
 const usePlaylist = () => {
+  /**
+   * @description State management for playlists, favorites, and recent playlists.
+   */
   const [state, setState] = useState({
     playlists: {},
     recentPlaylists: [],
     favourites: [],
   });
 
+  /**
+   * @description Fetch a playlist by ID from the API.
+   * @param {string} playlistId - ID of the playlist to fetch.
+   * @param {boolean} force - Force fetching even if the playlist is in the state.
+   */
   const getPlaylistById = async (playlistId, force = false) => {
+    // If playlist already exists in state and force is false, return
     if (state.playlists[playlistId] && !force) {
       return;
     }
 
     let result = await getPlaylist(playlistId);
-    let cid, ct;
+    let cid, ct; // cid and ct are used to store the channel ID and channel title of the playlist.
+
     result = result.map((item) => {
+      /**
+       * @description Mapping API response to a simplified object. 
+       */
       const {
         channelId,
         title,
@@ -42,34 +76,52 @@ const usePlaylist = () => {
       };
     });
 
+
+    /**
+     * @description Updating state with the fetched playlist data.
+     */
     setState((pre) => ({
       ...pre,
       playlists: {
         ...pre.playlists,
         [playlistId]: {
-          itens: result,
-          playlistId,
-          channelId: cid,
-          channelTitle: ct,
+          items: result, // Array of simplified playlist items
+          playlistId, // ID of the playlist
+          channelId: cid, // Channel ID of the playlist
+          channelTitle: ct, // Channel title of the playlist
         },
       },
     }));
   };
 
+
+  /**
+   * @description Add a playlist to the favorites list.
+   * @param {string} playlistId - ID of the playlist to add to favorites.
+   */
   const addToFavorites = (playlistId) => {
     setState((pre) => ({
       ...pre,
-      favourites: [...pre, playlistId],
+      favourites: [...pre.favourites, playlistId],
     }));
   };
 
+  /**
+   * @description Add a playlist to the recent playlists list.
+   * @param {string} playlistId - ID of the playlist to add to recent playlists.
+   */
   const addToRecent = (playlistId) => {
     setState((pre) => ({
       ...pre,
-      recentPlaylists: [...pre, playlistId],
+      recentPlaylists: [...pre.recentPlaylists, playlistId],
     }));
   };
 
+  /**
+   * @description Get an array of playlists by their IDs.
+   * @param {Array} ids - Array of playlist IDs to retrieve.
+   * @returns {Array} - Array of playlists.
+   */
   const getPlaylistByIds = (ids = []) => {
     return ids.map((id) => state.playlists[id]);
   };
